e192652265e172de8defdbda3764a904faa337ba
hulizhong <hulizhong@yeah.net>
2017-03-10 15:50:48 +0800

    Add, cmake and project init

---
+++ client/main.cpp
@@ -0,0 +1,19 @@
+/*************************************************************************
+	> File Name: main.cpp
+	> Author: 
+	> Mail: 
+	> Created Time: Saturday, March 04, 2017 PM04:57:31 HKT
+ ************************************************************************/
+
+#include<iostream>
+using namespace std;
+
+
+
+int main(void)
+{
+    std::cout << "this is EchoClient " << std::endl;
+
+    return 0;
+}
+
